68f9633ed08f7e1daa57703500ed96bf2801b395
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2023-05-25 05:30:29 +0200

    fix: Specify profile for tests

--- src/test/java/fr/univrouen/stb23v1/Stb23v1ApplicationTests.java
+++ src/test/java/fr/univrouen/stb23v1/Stb23v1ApplicationTests.java
@@ -2,8 +2,10 @@ package fr.univrouen.stb23v1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class Stb23v1ApplicationTests {
 
 	@Test
